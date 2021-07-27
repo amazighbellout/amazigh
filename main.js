13c0e12bb2e5b6ed5504c451c8c2c99a3df2cc28
amazighbellout <88006627+amazighbellout@users.noreply.github.com>
2021-07-27 11:24:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+
+function setUserName() {
+  let myName = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', myName);
+  myHeading.textContent = 'Mozilla est cool, ' + myName;
+}
+
+if (!localStorage.getItem('nom')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('nom');
+  myHeading.textContent = 'Mozilla est cool, ' + storedName;
+}
+myButton.addEventListener('click', function() {
+  setUserName();
+});
+
+
+let myImage = document.querySelector('img');
+
+
+
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/amazigh.jpg') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/amazigh.jpg');
+    }
+});
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc != 'images/amazigh.jpg') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/amazigh.jpg');
+    }
+});
